488055c6731cee9a1139fdc40a939690a0c5bd19
Alaere00 <alaere@pdx.edu>
2023-01-29 12:25:33 -0800

    Initial Commit

---
+++ MangaTranslator/MangaTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaTranslatorApp.swift
+//  MangaTranslator
+//
+//  Created by Alaere Nekekpemi on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MangaTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
